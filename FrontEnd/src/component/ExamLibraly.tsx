5a5a819b08b3e49141e429203d1df1ed2b5d726d
kientt265 <kientran26503@gmail.com>
2024-10-03 22:42:50 +0700

    container

---
+++ FrontEnd/src/component/ExamLibraly.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ExamLibraly = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                IELTS Exam Library
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ExamLibraly
