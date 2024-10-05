72e29277d687a2421f76eeb0e9a29295a660793e
kientt265 <kientran26503@gmail.com>
2024-10-05 13:29:22 +0700

    add-routing

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import Header from './component/Header'
 import Display from './component/Display'
 import Footer from './component/Footer'
-
+import Main from './component/Main'
 function App() {
   
 
   return (
     <div className='flex flex-col'>
-    <Header/>
-    <Display/>
+    <Header onLoginClick={() => console.log('Login clicked')} onSignUpClick={() => console.log('Sign up clicked')} onHomeClick={() => console.log('Home')}/>
+    <Main/>
     <Footer/>
     </div>
   )
--- FrontEnd/src/component/Header.tsx
+++ FrontEnd/src/component/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-
-const Header = () => {
+interface HeaderProps {
+    onLoginClick: () => void;
+    onSignUpClick: () => void;
+    onHomeClick: () => void;
+  }
+const Header: React.FC<HeaderProps> = ({ onLoginClick, onSignUpClick, onHomeClick }) => {
   return (
     <div className='fixed top-0 left-0 w-full z-50 bg-slate-200'>
         <div className='flex  bg-white-300 p-3 items-center'>
-        <div className='flex gap-1 w-[20%] ml-5'>
+        <div className='flex gap-1 w-[20%] ml-5 cursor-pointer' onClick={onHomeClick}>
             <h1 className='font-bold text-blue-700'>Fantasy</h1>
             <h1 className='font-bold'>IELTS</h1>
         </div>
@@ -15,10 +19,10 @@ const Header = () => {
             <a href="#">About Us</a>
         </div>
         <div className='flex justify-around items-center w-[20%]'>
-            <p className='font-bold cursor-pointer'>Sign In</p>
-            <div>
-                <p className='font-bold bg-[#525FE1] rounded p-2 cursor-pointer'>Sign Up</p>
-            </div>
+                <p className='font-bold cursor-pointer bg-[#FFFFFF] rounded p-2 ' onClick={onLoginClick}>Log In</p>
+                <div>
+                <p className='font-bold bg-[#525FE1] rounded p-2 cursor-pointer' onClick={onSignUpClick}>Sign Up</p>
+                </div>
         </div>
         </div>
     </div>
